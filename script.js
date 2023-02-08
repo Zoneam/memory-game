c87aa00577e24aaf4c115b5c78e17510adece6b5
Zoneam <zoneam@gmail.com>
2023-02-08 00:23:41 -0800

    refactoring code, styling game board

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-
 let flippedCards = [];
 let flippedCardIds = [];
 let score = 0;
@@ -10,36 +9,24 @@ let decrementButton = document.getElementById("decrementButton");
 let winMessage = document.querySelector('#win-message');
 
 function nextPerfectSquare(num) {
-    let i = 1;
-    while (i * i <= num) i++;
-    if(i % 2 == 0) return i * i;
-    else return (i + 1) * (i + 1);
-  }
+    if(num > 28) num = 28;
+    return (num + 1 % 2 == 0) ? Math.pow(num + 1,2) : Math.pow(num + 2, 2);
+}
 
-  function prevPerfectSquare(num) {
-    let i = 1;
-    while (i * i < num) i++;
-    if((i-1) % 2 == 0) return (i-1) * (i-1);
-    else return (i - 2) * (i - 2);
-  }
+function prevPerfectSquare(num) {
+    if(num < 4) num = 4;
+    return (num - 1 % 2 == 0) ? Math.pow(num - 1,2) : Math.pow(num - 2, 2);
+}
 
   incrementButton.addEventListener("click", function() {
-    score = 0;
-    flippedCardIds = [];
-    flippedCards = [];
     let current = parseInt(inputField.value);
-    inputField.value = nextPerfectSquare(current);
-    inputField.step = nextPerfectSquare(current) - current;
+    inputField.value = nextPerfectSquare(Math.sqrt(current));
     renderDeck(inputField.value);
   });
 
   decrementButton.addEventListener("click", function() {
-    score = 0;
-    flippedCardIds = [];
-    flippedCards = [];
     let current = parseInt(inputField.value);
-    inputField.value = prevPerfectSquare(current);
-    inputField.step = current - prevPerfectSquare(current);
+    inputField.value = prevPerfectSquare(Math.sqrt(current));
     renderDeck(inputField.value);
   });
 
@@ -68,7 +55,11 @@ function createCard(id, value) {
 // Render the deck of cards
 function renderDeck(tiles) {
     if (timer) clearInterval(timer);
+    score = 0;
+    flippedCardIds = [];
+    flippedCards = [];
     const cards = [];
+    winMessage.innerText = "";
     for (let i = 0; i < tiles/2; i++) {
         cards.push(i,i);
     }
@@ -81,6 +72,7 @@ function renderDeck(tiles) {
     allCards.forEach((card) => {
         card.style.width = `${800/Math.sqrt(tiles)}px`;
         card.firstElementChild.querySelector('h1').style.fontSize = `${(800/Math.sqrt(tiles)/10*4)}px`;
+        card.children[1].querySelector('h1').style.fontSize = `${(800/Math.sqrt(tiles)/10*4)}px`;
         card.addEventListener('click', flipCard);
     });
     timer = setInterval(hint, 800/tiles*40);
